fe9c247f82d6b4ef362e6389f567873dfea4aee4
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-12-15 21:26:18 +0530

    Add custom-pop polyfill for array

---
+++ polyfills/custom-pop.js
@@ -0,0 +1,20 @@
+function customPop() {
+  if (this.length === 0) {
+    return undefined;
+  }
+
+  const poppedElement = this[this.length - 1];
+  // this.splice(this.length-1, 1);
+
+  // another way of doing it
+  this.length = this.length - 1;
+
+  return poppedElement;
+}
+
+Array.prototype.customPop = customPop;
+
+const arr = [1, 2, 3, 4, 5, 6, 7];
+console.log(arr.customPop())
+console.log(arr.customPop())
+console.log(arr)
