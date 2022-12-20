21acaa411ffc2c4a578da7194890bbf44fe28a8f
M-o-h-a-n-S-i-n-g-h <mohan.singh1385664@gmail.com>
2022-12-20 21:30:59 +0530

    Modified to handle edge cases

--- polyfills/custom-includes.js
+++ polyfills/custom-includes.js
@@ -1,22 +1,43 @@
 function customIncludes(searchElement) {
-  if (this.length === 0) return false;
-  
-  if (this === null || this === undefined) {
-    throw new TypeError('Array.prototype.customIncludes called on null or undefined');
+  let thisValue = this
+  if (thisValue === 0) return false
+
+  if (thisValue === null || thisValue === undefined) {
+    throw new TypeError('Array.prototype.customIncludes called on null or undefined')
   }
-  
-  if (!Array.isArray(this)) {
+
+  if (
+    Object.prototype.toString.call(thisValue) === '[object Object]' &&
+    thisValue.hasOwnProperty('length')
+  ) {
+    thisValue = Object.entries(thisValue)
+      .map(([key, value]) => {
+        if (key !== 'length') return value
+      })
+      .filter(Boolean)
+  } else if (!Array.isArray(thisValue)) {
     throw new TypeError('Cannot call on non-array objects')
   }
-  
-  for (const element of this) {
-    if (element === searchElement) return true;
-    if (element !== element && searchElement !== searchElement) return true;
+
+  let fromIndex = parseInt(arguments[1]) || 0
+  if (fromIndex >= thisValue.length) return false
+
+  if (fromIndex < 0 && Math.abs(fromIndex) < thisValue.length) {
+    fromIndex = thisValue.length + fromIndex
+  } else {
+    fromIndex = 0
   }
-  
-  return false;
+
+  for (let i = fromIndex; i < thisValue.length; i++) {
+    if (thisValue[i] === searchElement) return true
+    if (thisValue[i] !== thisValue[i] && searchElement !== searchElement) return true
+  }
+
+  return false
 }
 
-Array.prototype.customIncludes = customIncludes;
-const arr = [1, 2, 3, 7, NaN];
-console.log(arr.customIncludes("w"));
+Array.prototype.customIncludes = customIncludes
+// const arr = [1, 3, 12, 2, NaN]
+// console.log(arr.customIncludes(NaN))
+const arr = [1, 3, 12, 2]
+console.log(arr.customIncludes(12))
